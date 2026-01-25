cc65efb32fb1060cb7ae8fc9b24011a1490dec25
Mohammed <mohammedabbas@Mohammeds-MacBook-Pro.local>
2026-01-25 08:03:50 +0000

    Initial commit - Login system

---
+++ roomradar/roomradar/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def loginView(request):
+    if request.method == "POST":
+        username = request.POST['username'].strip.lower()
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+    return render(request, 'login.html')
+
+def dashboardView(request):
+    return render(request,'dashboard.html')
