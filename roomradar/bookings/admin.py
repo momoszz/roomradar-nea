b33c26fa141d14112c0e43110842b2af9ac78868
momoszz <justanemail9078@gmail.com>
2026-02-09 06:28:32 +0000

    Add code comments to admin.py for Iteration 1 documentation

--- roomradar/bookings/admin.py
+++ roomradar/bookings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Room, Booking
 
+# Register models so they appear in Django admin panel for testing
 admin.site.register(Room)
 admin.site.register(Booking)
