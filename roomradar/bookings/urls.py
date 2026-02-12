7b0957b0404a4e11fbcf1bd05c47eb60507e7374
momoszz <justanemail9078@gmail.com>
2026-02-12 11:47:59 +0000

    Fix urls.py comments to be lowercase

--- roomradar/bookings/urls.py
+++ roomradar/bookings/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-# URL routing for bookings app
+# url routing for bookings app
 urlpatterns = [
-    path('', views.loginView, name='login'),  # Root URL shows login page
+    path('', views.loginView, name='login'),  # root url shows login page
     path('dashboard/teacher/', views.teacherDashboard, name='teacherDashboard'),
     path('dashboard/admin/', views.adminDashboard, name='adminDashboard'),
 ]
